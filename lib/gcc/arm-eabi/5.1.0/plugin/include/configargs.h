96713ce3029b85d09c5772cde98f438c0c8974c9
liminghao <liminghao@xiaomi.com>
2015-07-08 17:45:22 +0800

    arm-eabi-*: add arm-eabi-* toolchains for build linux kernel.
    N/A
    
    sources: https://github.com/gcc-toolchains
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

---
+++ lib/gcc/arm-eabi/5.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/liminghao/big/leeminghao/gcc-toolchains/gcc/gcc-5.1.0/configure --target=arm-eabi --prefix=/home/liminghao/big/leeminghao/gcc-toolchains/out/arm-eabi/arm-eabi-5.1.0 --build=x86_64-linux-gnu --host=x86_64-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-gmp=/home/liminghao/big/leeminghao/gcc-toolchains/out/arm-eabi/temp-install --with-mpfr=/home/liminghao/big/leeminghao/gcc-toolchains/out/arm-eabi/temp-install --with-mpc=/home/liminghao/big/leeminghao/gcc-toolchains/out/arm-eabi/temp-install --with-isl=/home/liminghao/big/leeminghao/gcc-toolchains/out/arm-eabi/temp-install --disable-isl-version-check --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-libitm --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --with-abi=aapcs --disable-shared --disable-tls";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs" }, { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
